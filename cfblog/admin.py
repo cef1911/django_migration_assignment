12c832d07442cef40ebf5cd5b8a15a234a1763d5
chris franklin <cef1911@hotmail.com>
2021-06-02 19:53:52 -0400

    6/2/2021 7:53pm commit

--- cfblog/admin.py
+++ cfblog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import School, Student
+from .models import School, Student, Certificate_Type, Department, Grade, Faculty
 
 # Register your models here.
 
 admin.site.register(School)
 admin.site.register(Student)
+admin.site.register(Certificate_Type)
+admin.site.register(Department)
+admin.site.register(Grade)
+admin.site.register(Faculty)
--- cfblog/models.py
+++ cfblog/models.py
@@ -1,9 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.utils import timezone
 
 # Create your models here.
 
-class Student(models.Model):
-    pass
-
 class School(models.Model):
-    pass
+
+    name = models.CharField(max_length= 400, default="", null=True)
+    def __str__(self):
+        return self.name
+   
+
+class Certificate_Type(models.Model):
+   
+    name = models.CharField(max_length= 50)
+    def __str__(self):
+        return self.name
+
+class Faculty(models.Model):
+
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class Department(models.Model):
+
+    name = models.CharField(max_length=50)
+    faculty = models.ForeignKey(Faculty, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Grade(models.Model):
+
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    full_name = models.CharField(max_length= 50, null=True)
+    year_of_graduation = models.DateField(default=timezone.now)
+    department = models.ForeignKey(Department, on_delete=CASCADE, null=True)
+    grade = models.ForeignKey(Grade, on_delete=CASCADE, null=True)
+    school = models.ForeignKey(School,on_delete=CASCADE, null=True)
+    certificate_type  = models.ForeignKey(Certificate_Type, on_delete=CASCADE, null=True)
+
+    def __str__(self):
+        return self.full_name
